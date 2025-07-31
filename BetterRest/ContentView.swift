d37e01b7ba1c4dffc62da7666f1d34203020ebc9
sougatoroy3 <sougatoroy007@gmail.com>
2025-07-31 19:34:52 +0530

    Initital Commit

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in:4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp)
+                .labelsHidden()
         }
         .padding()
     }
